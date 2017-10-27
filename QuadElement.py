c197ef20b00e9f6cfece2807d9487a230512dd42
Tim Kuipers <t.kuipers@ultimaker.com>
2017-10-27 16:33:52 +0200

    solve jacian bugs
    transposed and wrong sign in M matrix

--- QuadElement.py
+++ QuadElement.py
@@ -10,8 +10,6 @@ class QuadElement:
         n = self.elem_count
         self.material = material
         self.global_coords = global_coords.reshape(-1,1)
-        self.T_nodal = np.matrix([[1,2],[3,4]]) # TODO: make depend on coords in the quad!
-        self.K = np.identity(d*n) # local stiffness matrix
             
     def getGlobalStiffnessMatrix(self):
         result = MathTK.do2DimensionalGaussLegendreIntegration(self.toBeIntegrated, self.integration_steps)
@@ -26,10 +24,14 @@ class QuadElement:
         return B.transpose().dot(D).dot(B) * (det * t)
     
     def getB(self, r, s):
-        jacobian = self.getJacobian(r, s)
-        A = self.getA(jacobian)
         shape_function_derivatives = self.getShapeFunctionDerivatives(r, s)
         M = self.getM(shape_function_derivatives)
+        jacobian = self.getJacobian(M)
+        #print("shape_function_derivatives:" + str(shape_function_derivatives))
+        #print("coords: " +str(self.global_coords))
+        #print("M:" + str(M))
+        #print("j: " + str(jacobian))
+        A = self.getA(jacobian)
         return A.dot(M)
     
     # the jacobian for the strain
@@ -44,34 +46,37 @@ class QuadElement:
         assert(j.shape == (d, d))
         #print("J: " + str(j))
         #print("det_J: " +str(det_J))
-        return np.matrix([[j[1,1], -j[0,1], 0, 0],
-                            [0, 0, -j[1,0], j[0,0]],
-                            [-j[1,0], j[0,0], j[1,1], j[0,1]]
-                            ]) # * (1 / self.det_J)
-    
-    # not 100% sure
-    def getJacobian(self, r, s):
+        return np.matrix([  [ j[1,1], -j[0,1],       0,       0], # ( du/dr )
+                            [      0,       0, -j[1,0],  j[0,0]], # ( du/ds )
+                            [-j[1,0],  j[0,0],  j[1,1], -j[0,1]]  # ( dv/ds )
+                            ])  * (1 / self.det_J)               # ( dv/dr )
+
+    def getJacobian(self, M):
         d = QuadElement.dim_count
-        shape_function_derivatives = QuadElement.getShapeFunctionDerivatives(r, s)
-        M = QuadElement.getM(shape_function_derivatives)
         derivatives = M.dot(self.global_coords)
-        ret = derivatives.reshape(d, d)
+        # dx/dr
+        # dx/ds
+        # dy/dr
+        # dy/ds
+        ret = derivatives.reshape(d, d).transpose()
         self.det_J = np.linalg.det(ret)
-        #print("coords: " +str(self.global_coords))
-        #print("shape_function_derivatives:" + str(shape_function_derivatives))
-        #print("M:" + str(M))
-        #print("j: " + str(ret))
         return ret
     
     @staticmethod
     def getM(shape_function_derivatives):
         d = QuadElement.dim_count
         n = QuadElement.elem_count
-        (dNdr, dNds) = shape_function_derivatives
+        shape_function_derivatives
         ret = np.zeros((d * d, n * d))
         for xydim in range(d):
-            ret[xydim * d + 0, xydim::d] = dNdr
-            ret[xydim * d + 1, xydim::d] = dNds
+            for rsdim in range(d):
+                ret[xydim * d + rsdim, xydim::d] = shape_function_derivatives[rsdim]
+                #              v v v v v v v v v v v v v
+                #
+                # ( du/dr )   [ dN1dr   0   dN2dr   0   ] ( u1_x )
+                # ( du/ds ) = [ dN1ds   0   dN2ds   0   ] ( u1_y )
+                # ( dv/ds )   [   0   dN1dr   0   dN2dr ] ( u2_x )
+                # ( dv/dr )   [   0   dN1ds   0   dN2ds ] ( u2_y )
         return ret
     
     @staticmethod
